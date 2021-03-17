a4fd82b8f1ea8bec9797966cd6d85489e0d9911e
Roman Lymar <r.v.lymar@gmail.com>
2021-03-17 22:23:35 +0200

    created package template, custom, default + repository

---
+++ src/main/java/com/pasha/springboot/cassandraproject/domains/PackageDefault.java
@@ -0,0 +1,8 @@
+package com.pasha.springboot.cassandraproject.domains;
+
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("package_default")
+public class PackageDefault extends PackageTemplate{
+    
+}
--- src/main/java/com/pasha/springboot/cassandraproject/services/PackageService.java
+++ src/main/java/com/pasha/springboot/cassandraproject/services/PackageService.java
@@ -1,6 +1,6 @@
 package com.pasha.springboot.cassandraproject.services;
 
-import com.pasha.springboot.cassandraproject.domains.Package;
+import com.pasha.springboot.cassandraproject.domains.PackageTemplate;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -8,10 +8,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface PackageService {
-    Iterable<Package> getAllPackages();
-    Optional<Package> getPackageById(UUID id);
-    List<Package> getPackagesByNames(List<String> names);
-    Package saveProduct(Package aPackage);
+    Iterable<PackageTemplate> getAllPackages();
+    Optional<PackageTemplate> getPackageById(UUID id);
+    List<PackageTemplate> getPackagesByNames(List<String> names);
+    PackageTemplate saveProduct(PackageTemplate aPackageTemplate);
     BigDecimal getCost(List<UUID> ids);
     void deleteProduct(UUID id);
 }
