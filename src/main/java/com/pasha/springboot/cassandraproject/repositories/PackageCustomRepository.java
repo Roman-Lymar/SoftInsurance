e682da35456aabe7d7878ca1399ce8c258221ffd
Trofymov Pavel <paveltrof86@gmail.com>
2021-03-24 13:46:37 +0200

    Cassandra Astra and Heroku

--- src/main/java/com/pasha/springboot/cassandraproject/repositories/PackageCustomRepository.java
+++ src/main/java/com/pasha/springboot/cassandraproject/repositories/PackageCustomRepository.java
@@ -2,10 +2,12 @@ package com.pasha.springboot.cassandraproject.repositories;
 
 import com.pasha.springboot.cassandraproject.domains.PackageCustom;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
+@RestResource(exported = false)
 public interface PackageCustomRepository extends CassandraRepository<PackageCustom, UUID> {
 }
