87d81fe75267291754f291b72f37bf969515938f
oksana211 <oksana.veretina222@gmail.com>
2021-04-09 18:49:45 +0300

    cors

--- src/main/java/com/springboot/cassandraproject/CassandraProjectApplication.java
+++ src/main/java/com/springboot/cassandraproject/CassandraProjectApplication.java
@@ -2,6 +2,14 @@ package com.springboot.cassandraproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class CassandraProjectApplication {
@@ -11,4 +19,19 @@ public class CassandraProjectApplication {
         SpringApplication.run(CassandraProjectApplication.class, args);
 
     }
+
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
 }
