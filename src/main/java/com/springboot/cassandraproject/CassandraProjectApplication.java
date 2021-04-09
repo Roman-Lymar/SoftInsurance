324aaee6001ff00285b0bdbe563395124cc67c1e
Trofymov Pavel <paveltrof86@gmail.com>
2021-04-09 17:44:32 +0300

    Beta9 security

--- src/main/java/com/springboot/cassandraproject/CassandraProjectApplication.java
+++ src/main/java/com/springboot/cassandraproject/CassandraProjectApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CassandraProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello Noda!");
         SpringApplication.run(CassandraProjectApplication.class, args);
+
     }
 }
